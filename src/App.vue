5145565166e26dc2180c36a79b1c717e5d8bd92d
jamie <jbuckley760@gmail.com>
2020-07-11 21:55:53 +0100

    Add google login

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,68 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <Header 
+    v-bind:profile="profile"
+    v-on:clickSignIn="signIn"
+    v-on:clickSignOut="signOut"/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/Header.vue';
 
 export default {
-  name: 'App',
+  data() {
+    return {
+      profile: null
+    };
+  },
+  mounted() {
+    /* eslint-disable */
+    gapi.load('auth2', () => {
+      const auth2 = gapi.auth2.init({
+        client_id: '846834681157-4heibg4re26art8qh89sr0334mjtfrea.apps.googleusercontent.com',
+        fetch_basic_profile: true,
+        scope: 'profile'
+      });
+
+      const authInstance = gapi.auth2.getAuthInstance();
+      authInstance.then(() => {
+        if (authInstance.isSignedIn.get() == true) {
+          console.log('is signed in');
+          this.setProfile(authInstance.currentUser.get().getBasicProfile());
+        }
+      })
+    });
+    /* eslint-enable */
+  },
+  methods: {
+    signIn() {
+      /* eslint-disable */
+      const auth2 = gapi.auth2.getAuthInstance();
+      auth2.signIn().then(() => {
+        this.setProfile(auth2.currentUser.get().getBasicProfile());
+      });
+      /* eslint-enable */
+    },
+    signOut() {
+      /* eslint-disable */
+      var auth2 = gapi.auth2.getAuthInstance();
+      auth2.signOut().then(() => {
+        this.profile = null;
+      });
+      /* eslint-enable */
+    },
+    setProfile(profile) {
+      this.profile = {
+        name: profile.getName(),
+        email: profile.getEmail(),
+        imageUrl: profile.getImageUrl()
+      }
+    }
+  },
   components: {
-    HelloWorld
+    Header
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,36 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand" href="#">TimeTrack</a>
+    <button
+      class="navbar-toggler"
+      type="button"
+      data-toggle="collapse"
+      data-target="#navbarSupportedContent"
+      aria-controls="navbarSupportedContent"
+      aria-expanded="false"
+      aria-label="Toggle navigation"
+    >
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav mr-auto"></ul>
+      <div class="my-2 my-lg-0">
+        <div v-if="profile">Signed in as {{profile.name}}
+          <button v-on:click="$emit('clickSignOut')">Sign Out</button>  
+        </div>
+        <button v-else id="google-signin-button" v-on:click="$emit('clickSignIn')">Sign In</button>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<style lang="scss">
+  @import "./header.scss";
+</style>
+
+<script>
+export default {
+  props: ['profile', 'signIn', 'signOut']
+};
+</script>
