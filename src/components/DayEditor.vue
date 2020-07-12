117efc4d8da051bcd7350995b80a4d40e95fb80a
jamie <jbuckley760@gmail.com>
2020-07-12 23:21:43 +0100

    Remove form on day editor

--- src/components/DayEditor.vue
+++ src/components/DayEditor.vue
@@ -15,34 +15,32 @@
       </tbody>
     </table>
 
-    <form>
-      <div class="form-group">
-        <label for="project">Project</label>
-        <select class="form-control" v-model="newTimeEntry.projectName">
-          <option v-for="project in projects" :key="project.name">{{project.name}}</option>
-        </select>
-      </div>
+    <div class="form-group">
+      <label for="project">Project</label>
+      <select class="form-control" v-model="newTimeEntry.projectName">
+        <option v-for="project in projects" :key="project.name">{{project.name}}</option>
+      </select>
+    </div>
 
-      <div class="row">
-        <div class="col">
-          <div class="form-group">
-            <label for="project">Number of units</label>
-            <input class="form-control" v-model="newTimeEntry.amount" placeholder="Number of units" />
-          </div>
-        </div>
-        <div class="col">
-          <div class="form-group">
-            <label for="project">Unit type</label>
-            <select class="form-control" v-model="newTimeEntry.unit">
-              <option value="days" selected="selected">Days</option>
-              <option value="hours">Hours</option>
-            </select>
-          </div>
+    <div class="row">
+      <div class="col">
+        <div class="form-group">
+          <label for="project">Number of units</label>
+          <input class="form-control" v-model="newTimeEntry.amount" placeholder="Number of units" />
         </div>
       </div>
+      <div class="col">
+        <div class="form-group">
+          <label for="project">Unit type</label>
+          <select class="form-control" v-model="newTimeEntry.unit">
+            <option value="days" selected="selected">Days</option>
+            <option value="hours">Hours</option>
+          </select>
+        </div>
+      </div>
+    </div>
 
-      <button class="btn btn-primary ml-md-3 float-right" v-on:click="onSaveTimeClicked">Add</button>
-    </form>
+    <button class="btn btn-primary ml-md-3 float-right" v-on:click="onSaveTimeClicked">Add</button>
   </div>
 </template>
 
