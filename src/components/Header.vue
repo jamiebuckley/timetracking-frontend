78a4e889a8876ca9ef5e1374e81bcd1a1c757bec
Jamie Buckley <Jamie.Buckley@wwutilities.co.uk>
2020-07-19 00:12:25 +0100

    Remove navbar collapse

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,19 +1,8 @@
 <template>
   <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <a class="navbar-brand" href="#">TimeTrack</a>
-    <button
-      class="navbar-toggler"
-      type="button"
-      data-toggle="collapse"
-      data-target="#navbarSupportedContent"
-      aria-controls="navbarSupportedContent"
-      aria-expanded="false"
-      aria-label="Toggle navigation"
-    >
-      <span class="navbar-toggler-icon"></span>
-    </button>
 
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <div class="" id="navbarSupportedContent">
       <ul class="navbar-nav mr-auto"></ul>
       <div class="my-2 my-lg-0">
         <div v-if="profile">Signed in as {{profile.name}}
