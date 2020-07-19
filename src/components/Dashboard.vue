d97a4a3f237604d9370bf80d31bdbb1385d1c1b6
Jamie Buckley <Jamie.Buckley@wwutilities.co.uk>
2020-07-19 14:11:09 +0100

    Add loading spinner and animations

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -1,163 +1,195 @@
 <template>
-  <div class="container-fluid py-md-5 py-3">
-    <ul class="nav nav-tabs mb-3">
-      <li class="nav-item">
-        <a
-          class="nav-link"
-          href="#"
-          v-on:click="tab = 'time'"
-          v-bind:class="{ active: (tab == 'time')}"
-        >Time</a>
-      </li>
-      <li class="nav-item">
-        <a
-          class="nav-link"
-          href="#"
-          v-on:click="tab = 'projects'"
-          v-bind:class="{ active: (tab == 'projects')}"
-        >Projects</a>
-      </li>
-    </ul>
-    <div v-if="tab == 'time'">
-      <div class="row">
-        <div class="col ml-md-3">
-          <TimeEditor
-            v-bind:onDaySelected="onDaySelected"
-            v-bind:syncProjects="syncProjects"
-            v-bind:currentMonth="startOfMonth"
-            v-bind:moveBackMonth="moveBackMonth"
-            v-bind:moveForwardMonth="moveForwardMonth"
-            v-bind:projects="projects"
-            v-bind:times="times"
-          />
+    <div>
+        <div v-if="isLoading">
+            <h4 class="d-flex justify-content-center my-5">Loading, please wait</h4>
+            <div class="d-flex justify-content-center my-5">
+                <div class="spinner-border" role="status">
+                    <span class="sr-only">Loading...</span>
+                </div>
+            </div>
         </div>
-        <div class="col dayEditor" v-bind:class="{ slideIn: !!selectedDay }">
-          <DayEditor
-            v-bind:selectedDay="selectedDay"
-            v-bind:projects="projects"
-            v-bind:saveTime="saveTime"
-            v-bind:timesForDay="timesForDay"
-            v-bind:deleteTimeEntry="deleteTimeEntry"
-            v-bind:cancelDaySelection="cancelDaySelection"
-            v-if="selectedDay"
-          />
+        <div class="container-fluid py-md-5 py-3 fade-in" v-if="!isLoading">
+            <ul class="nav nav-tabs mb-3">
+                <li class="nav-item">
+                    <a
+                            class="nav-link"
+                            href="#"
+                            v-on:click="tab = 'time'"
+                            v-bind:class="{ active: (tab == 'time')}"
+                    >Time</a>
+                </li>
+                <li class="nav-item">
+                    <a
+                            class="nav-link"
+                            href="#"
+                            v-on:click="tab = 'projects'"
+                            v-bind:class="{ active: (tab == 'projects')}"
+                    >Projects</a>
+                </li>
+            </ul>
+            <div v-if="tab == 'time'">
+                <div class="row">
+                    <div class="col ml-md-3">
+                        <TimeEditor
+                                v-bind:onDaySelected="onDaySelected"
+                                v-bind:syncProjects="syncProjects"
+                                v-bind:currentMonth="startOfMonth"
+                                v-bind:moveBackMonth="moveBackMonth"
+                                v-bind:moveForwardMonth="moveForwardMonth"
+                                v-bind:projects="projects"
+                                v-bind:times="times"
+                        />
+                    </div>
+                    <div class="col dayEditor" v-bind:class="{ slideIn: !!selectedDay }">
+                        <DayEditor
+                                v-bind:selectedDay="selectedDay"
+                                v-bind:projects="projects"
+                                v-bind:saveTime="saveTime"
+                                v-bind:timesForDay="timesForDay"
+                                v-bind:deleteTimeEntry="deleteTimeEntry"
+                                v-bind:cancelDaySelection="cancelDaySelection"
+                                v-if="selectedDay"
+                        />
+                    </div>
+                </div>
+            </div>
+            <ProjectList v-if="tab == 'projects'" v-bind="{ projects, saveProject, deleteProject }"/>
         </div>
-      </div>
     </div>
-    <ProjectList v-if="tab == 'projects'" v-bind="{ projects, saveProject, deleteProject }" />
-  </div>
 </template>
 
 <style>
-  @media only screen and (max-width: 800px) {
-    @keyframes slide-in {
-      from {left: 100%}
-      to {left: 20%}
+    @keyframes fade-in {
+        from {
+            opacity: 0%
+        }
+        to {
+            opacity: 100%
+        }
     }
 
-    .dayEditor {
-      display:none;
-      position:fixed;
-      background:white;
-      height:100%;
-      padding-top: 25px;
-      top: 0px;
-      border-left: 1px black;
-      width: 80%;
-      left: 20%;
+    .fade-in {
+        animation-name: fade-in;
+        animation-duration: 0.5s;
+        animation-timing-function: ease-out;
     }
 
-    .slideIn {
-      display:block;
-      animation-name: slide-in;
-      animation-duration: 0.5s;
+    @media only screen and (max-width: 800px) {
+        @keyframes slide-in {
+            from {
+                left: 100%
+            }
+            to {
+                left: 20%
+            }
+        }
+
+        .dayEditor {
+            display: none;
+            position: fixed;
+            background: white;
+            height: 100%;
+            padding-top: 25px;
+            top: 0px;
+            border-left: 1px black;
+            width: 80%;
+            left: 20%;
+        }
+
+        .slideIn {
+            display: block;
+            animation-name: slide-in;
+            animation-duration: 0.5s;
+        }
     }
-  }
 </style>
 
 <script>
-import ApiService from "../apiService";
-import ProjectList from "./ProjectsList";
-import TimeEditor from "./TimeEditor";
-import DayEditor from "./DayEditor";
-import moment from "moment";
+    import ApiService from "../apiService";
+    import ProjectList from "./ProjectsList";
+    import TimeEditor from "./TimeEditor";
+    import DayEditor from "./DayEditor";
+    import moment from "moment";
 
-export default {
-  data() {
-    return {
-      startOfMonth: moment(),
-      endOfMonth: moment(),
-      tab: "time",
-      projects: [],
-      selectedDay: null,
-      times: []
+    export default {
+        data() {
+            return {
+                startOfMonth: moment(),
+                endOfMonth: moment(),
+                tab: "time",
+                projects: [],
+                selectedDay: null,
+                times: [],
+                isLoading: false
+            };
+        },
+        components: {
+            ProjectList,
+            TimeEditor,
+            DayEditor
+        },
+        mounted() {
+            this.startOfMonth = moment().startOf("month").utcOffset(0, true);
+            this.endOfMonth = moment().endOf("month").utcOffset(0, true);
+            this.resync();
+        },
+        methods: {
+            resync: async function () {
+                this.isLoading = true;
+                await Promise.all([this.fetchProjects(), this.fetchTimes()]);
+                this.isLoading = false;
+            },
+            fetchProjects: async function () {
+                const projects = await ApiService.getProjects();
+                this.projects = projects;
+            },
+            fetchTimes: async function () {
+                const times = await ApiService.queryTimeEntries(this.startOfMonth, this.endOfMonth);
+                this.times = times;
+            },
+            saveProject: async function (project) {
+                await ApiService.saveProject(project);
+                this.projects.push(project);
+            },
+            deleteProject: async function (projectName) {
+                await ApiService.deleteProject({name: projectName});
+                this.projects = this.projects.filter(p => p.name != projectName);
+            },
+            async saveTime(newTimeEntry) {
+                await ApiService.createTimeEntry({
+                    ...newTimeEntry,
+                    dateTime: this.selectedDay.date
+                });
+                this.fetchTimes();
+            },
+            async deleteTimeEntry(timeEntry) {
+                await ApiService.deleteTimeEntry(timeEntry.key, timeEntry.dateTime);
+                this.times = this.times.filter(t => t !== timeEntry);
+            },
+            onDaySelected(day) {
+                this.selectedDay = day;
+            },
+            syncProjects(projects) {
+                this.projects = projects;
+            },
+            moveBackMonth() {
+                this.startOfMonth = moment(this.startOfMonth).utcOffset(0, true).subtract(1, 'month');
+                this.endOfMonth = moment(this.endOfMonth).utcOffset(0, true).subtract(1, 'month');
+                this.resync();
+            },
+            moveForwardMonth() {
+                this.startOfMonth = moment(this.startOfMonth).utcOffset(0, true).add(1, 'month');
+                this.endOfMonth = moment(this.endOfMonth).utcOffset(0, true).add(1, 'month');
+                this.resync();
+            },
+            cancelDaySelection() {
+                this.selectedDay = null;
+            }
+        },
+        computed: {
+            timesForDay() {
+                return this.times.filter(t => moment(t.dateTime).isSame(moment(this.selectedDay.date), "day"));
+            }
+        }
     };
-  },
-  components: {
-    ProjectList,
-    TimeEditor,
-    DayEditor
-  },
-  mounted() {
-    this.startOfMonth = moment().startOf("month").utcOffset(0, true);
-    this.endOfMonth =  moment().endOf("month").utcOffset(0, true);
-    this.fetchProjects();
-    this.fetchTimes();
-  },
-  methods: {
-    fetchProjects: async function() {
-      const projects = await ApiService.getProjects();
-      this.projects = projects;
-    },
-    fetchTimes: async function() {
-      const times = await ApiService.queryTimeEntries(this.startOfMonth, this.endOfMonth);
-      this.times = times;
-    },
-    saveProject: async function(project) {
-      await ApiService.saveProject(project);
-      this.projects.push(project);
-    },
-    deleteProject: async function(projectName) {
-      await ApiService.deleteProject({ name: projectName });
-      this.projects = this.projects.filter(p => p.name != projectName);
-    },
-    async saveTime(newTimeEntry) {
-      await ApiService.createTimeEntry({
-        ...newTimeEntry,
-        dateTime: this.selectedDay.date
-      });
-      this.fetchTimes();
-    },
-    async deleteTimeEntry(timeEntry) {
-      await ApiService.deleteTimeEntry(timeEntry.key, timeEntry.dateTime);
-      this.times = this.times.filter(t => t !== timeEntry);
-    },
-    onDaySelected(day) {
-      this.selectedDay = day;
-    },
-    syncProjects(projects) {
-      this.projects = projects;
-    },
-    moveBackMonth() {
-      this.startOfMonth = moment(this.startOfMonth).utcOffset(0, true).subtract(1, 'month');
-      this.endOfMonth =  moment(this.endOfMonth).utcOffset(0, true).subtract(1, 'month');
-      this.fetchProjects();
-      this.fetchTimes();
-    },
-    moveForwardMonth() {
-      this.startOfMonth = moment(this.startOfMonth).utcOffset(0, true).add(1, 'month');
-      this.endOfMonth =  moment(this.endOfMonth).utcOffset(0, true).add(1, 'month');
-      this.fetchProjects();
-      this.fetchTimes();
-    },
-    cancelDaySelection() {
-      this.selectedDay = null;
-    }
-  },
-  computed: {
-    timesForDay() {
-      return this.times.filter(t => moment(t.dateTime).isSame(moment(this.selectedDay.date), "day"));
-    }
-  }
-};
 </script>
